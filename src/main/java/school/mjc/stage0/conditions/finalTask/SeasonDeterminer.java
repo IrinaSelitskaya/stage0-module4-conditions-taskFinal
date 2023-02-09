e1683f2c1a88b2ad81fe6f3479a47bddb5789e94
IrinaSelitskaya <119079534+IrinaSelitskaya@users.noreply.github.com>
2023-02-09 18:28:05 +0300

    Update SeasonDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -2,7 +2,6 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
-     monthNumber=13;
      if(monthNumber==12||monthNumber==1||monthNumber==2){
       System.out.println("Winter");} 
      else if(monthNumber>=3 && monthNumber<=5){
